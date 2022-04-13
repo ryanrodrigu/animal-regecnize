afec6dd9fed8a357cb8a144dcd44b7e8aaf83b2f
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-04-12 20:39:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/7ke_zOQcd/model.json', modelReady);
+    
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
