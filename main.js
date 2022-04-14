7ef5f943f408cf575b58e62a829b0e9012a7066a
ryanrodrigu <97711855+ryanrodrigu@users.noreply.github.com>
2022-04-13 20:55:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,59 @@ function startClassification()
 function modelReady(){
     classifier.classify(gotResults);
 }
+
+var dog = 0;
+var cat = 0;
+var cow = 0;
+var chicken = 0;
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+     } else{
+         console.log(results);
+         random_number_r = Math.floor(Math.random() * 255) + 1;
+         random_number_g = Math.floor(Math.random() * 255) + 1;
+         random_number_b = Math.floor(Math.random() * 255) + 1;
+
+
+         document.getElementById("result_label").innerHTML = 'Detected Voice Is Off - '+
+         results[0].label;
+         document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+'Detected Cat -'+cat+'Detected Cow -'+cow+'Detected Chicken -'+chicken;
+         
+         document.getElementById("result_label").style.color = "rgb("
+         +random_number_r+" , "+random_number_g+","+random_number_r+")";
+         document.getElementById("result_count").style.color="rgb("
+         +random_number_r+","+random_number_g+","+random_number_r+")";
+
+        img = document.getElementById("animal_image");
+        if(results[0].label == "Barking")
+        {
+            img.src ="dog.gif";
+            dog = dog+1;
+        }
+     else if(result[0].label =="Meowing")
+     {
+         img.src ="cat.gif";
+         cat = cat+1;
+     }
+     else if(result[0].label =="Mooing")
+     {
+         img.src ="cow.gif";
+         cow = cow+1;
+     }
+     else if(result[0].label =="Chicken Sound")
+     {
+         img.src ="chicken.jpeg";
+         chicken = chicken+1;
+     }
+     else{
+         img.src ="ear.png";
+     }
+
+     
+
+     }
+
+
+}
